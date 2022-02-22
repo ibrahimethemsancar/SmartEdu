a72a3b21835df3911cd571605f9121bbd76a065f
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-02-22 17:49:32 +0300

    edit links

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+ //Routes
 app.get("/", (req, res) => {
-  res.send("index sayfası");
+  res.status(200).render('index',{
+      page_name:'index'
+  });
 });
+app.get("/about", (req, res) => {
+    res.status(200).render('about',{
+        page_name:'about'
+    });
+  });
 
 const port = 3000;
 
