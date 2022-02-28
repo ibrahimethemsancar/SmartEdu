c8d0514f82d7d7b7bbd2b3bf683581df3924748d
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-02-28 17:38:52 +0300

    course model,controller and route

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const mongoose=require('mongoose');
+const bodyParser = require('body-parser')
 const pageRoute=require('./routes/pageRoute')
+const courseRoute=require('./routes/courseRoute')
 const app = express();
 
+//Connect DB
+mongoose.connect('mongodb://localhost/smartEdu-db')
+.then(()=>{
+  console.log("db connected")
+})
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static("public"));
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //Routes
 app.use("/",pageRoute);
+app.use("/courses",courseRoute);
 
 const port = 3000;
 
